079da744f9f574b929362307d97415d2c057d8b0
devaneyj3 <jordandevaney28@gmail.com>
2025-11-11 21:18:53 -0500

    create landing page with animations

---
+++ components/LandingPage.js
@@ -0,0 +1,31 @@
+"use client";
+import React, { useState } from "react";
+import classes from "../styles/Landing.module.scss";
+
+export default function LandingPage() {
+	const [name, setName] = useState("");
+	return (
+		<div className={classes.container}>
+			<div className={classes.matrix_container}>
+				<h1>Welecome to Simon Says</h1>
+				<div className={classes.rain}></div>
+			</div>
+			<div className={classes.nameInputs}>
+				<div>
+					<label className={classes.label} htmlFor="name">
+						What is your name?
+					</label>
+				</div>
+				<div>
+					<input
+						className={classes.input}
+						id="name"
+						type="text"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+			</div>
+			{name && <h1>Welcome, {name}</h1>}
+		</div>
+	);
+}
